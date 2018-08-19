64c4284267f5a367de6786b2db0f64caa22507bf
xieqiang <xieqiang@wdai.com>
2018-08-19 19:39:03 +0800

    去掉无用代码

--- src/main/java/com/example/controller/ConsumerController.java
+++ src/main/java/com/example/controller/ConsumerController.java
@@ -1,11 +1,9 @@
 package com.example.controller;
 
 import com.example.service.ConsumerService;
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -20,19 +18,10 @@ public class ConsumerController {
     @Autowired
     ConsumerService consumerService;
 
-    /**
-     * 采用缺省配置
-     * */
-    static {
-        BasicConfigurator.configure();
-    }
+
 
     @RequestMapping(value = "/test")
     public String add() {
-        int  a=100;
-        int  b=200;
-        int c= a+b;
-        System.out.println(c);
         consumerService.add(10,15);
         return  "success";
     }
